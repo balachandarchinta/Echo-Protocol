a0045beec26f5732082b3d0b02cf4b5049663c6f
balachandarchinta <balachandar.chinta@gmail.com>
2026-07-25 20:45:51 +0530

    Fix Vite deployment for Catalyst Web Client

--- echo-protocol-frontend/vite.config.js
+++ echo-protocol-frontend/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 const rawPort = process.env.ZC_SLATE_PORT || process.env.PORT
-const parsedPort = rawPort && !isNaN(parseInt(rawPort, 10)) ? parseInt(rawPort, 10) : 5174
+const parsedPort =
+  rawPort && !isNaN(parseInt(rawPort, 10))
+    ? parseInt(rawPort, 10)
+    : 5174
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   server: {
     port: parsedPort,
